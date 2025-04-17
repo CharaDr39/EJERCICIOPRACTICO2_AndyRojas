93c35ba7c8f95902af2ecaeb7bf969061777aedd
Chara <164945066+CharaDr39@users.noreply.github.com>
2025-04-16 20:43:39 -0600

    casi completo, solo faltan vistas

--- InitializrSpringbootProject/src/main/java/com/config/SecurityConfig.java
+++ InitializrSpringbootProject/src/main/java/com/config/SecurityConfig.java
@@ -1,34 +1,39 @@
 package com.config;
 
+import com.serviceimplementation.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import com.serviceimplementation.CustomUserDetailsService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     private final CustomUserDetailsService uds;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-          .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/peliculas/**", "/funciones/**").hasRole("ADMIN")
-            .requestMatchers("/reservas/**").hasAnyRole("USER","ADMIN")
-            .requestMatchers("/css/**","/js/**","/","/login").permitAll()
-            .anyRequest().authenticated()
-          )
-          .formLogin(form -> form
-            .loginPage("/login")
-            .defaultSuccessUrl("/", true)
-            .permitAll()
-          )
-          .logout(logout -> logout.permitAll());
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/css/**", "/js/**", "/img/**", "/", "/login", "/error").permitAll()
+                .requestMatchers("/reservas/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/peliculas/**", "/funciones/**", "/usuarios/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+            .formLogin(form -> form
+                .loginPage("/login")
+                .defaultSuccessUrl("/", true)
+                .permitAll())
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .logoutSuccessUrl("/")
+                .permitAll())
+            .userDetailsService(uds);
+
         return http.build();
     }
 
